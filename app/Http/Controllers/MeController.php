4aabcf824001eedcbe4def85cfda69f9b1446ed2
Luis Henrique <junimhs10@gmail.com>
2021-03-17 17:11:13 -0300

    update profile user

--- app/Http/Controllers/MeController.php
+++ app/Http/Controllers/MeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UpdateUserRequest;
 use App\Http\Resources\UserResource;
+use App\Services\MeService;
 use Illuminate\Http\Request;
 
 class MeController extends Controller
@@ -15,5 +17,13 @@ class MeController extends Controller
     public function index()
     {
         return new UserResource(auth()->user());
+    }
+
+    public function update(UpdateUserRequest $request)
+    {
+        $inputs = $request->validated();
+        $user = (new MeService())->update(auth()->user(), $inputs);
+
+        return new UserResource($user);
     }
 }
